fd5a64e91c4705d9cce973dda960f8d7bdfddafa
Sadra <alizadeh.sadra.gh@gmail.com>
2024-06-28 23:49:49 +0300

    Commiting main_1(1)

---
+++ App1_training/Flutter_train/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Image(
+          image: AssetImage('lib/assets/imagetest.jpg'),
+        ),
+      ),
+      backgroundColor: Colors.blueGrey[700],
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[900],
+        title: Text('Im rich'),
+      ),
+    ),
+  ));
+}
