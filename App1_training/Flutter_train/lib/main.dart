92d721fb10c02cdfb87e707d945b1b055e359055
Sadra <alizadeh.sadra.gh@gmail.com>
2024-06-29 14:14:08 +0300

    Learn Column&Row_1(3)

--- App1_training/Flutter_train/lib/main.dart
+++ App1_training/Flutter_train/lib/main.dart
@@ -10,14 +10,46 @@ void main() {
         // ),
         backgroundColor: Colors.blueGrey[700],
         body: SafeArea(
-          child:Container(
-            color: Colors.white,
-            child: Text("Hello my name is sadra"),
-            height: 100.0,
-            width: 220.0,
-            margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
-            padding: EdgeInsets.all(25.0),
-        )
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            verticalDirection: VerticalDirection.down,
+            children: [
+              Container(
+                color: Colors.white,
+                child: Text("Container 1"),
+                // height: 100.0,
+                width: 220.0,
+                margin: EdgeInsets.fromLTRB(10, 50, 0, 0),
+                padding: EdgeInsets.all(25.0),
+
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                color: Colors.red,
+                child: Text("Container 2"),
+                // height: 100.0,
+                width: 220.0,
+                margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                padding: EdgeInsets.all(25.0),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                color: Colors.blue,
+                child: Text("Container 3"),
+                // height: 100.0,
+                width: 220.0,
+                margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                padding: EdgeInsets.all(25.0),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+            ],
+          ),
         )
         ),
   ));
