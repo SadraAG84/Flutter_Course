943ef60729968a2712f26114ae6f80c94b05cf32
Sadra <alizadeh.sadra.gh@gmail.com>
2024-06-30 19:24:42 +0300

    Challenge completed(4)

--- App1_training/Flutter_train/lib/main.dart
+++ App1_training/Flutter_train/lib/main.dart
@@ -2,55 +2,112 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+      home: Scaffold(
         // body: Center(
         //   child: Image(
         //     image: AssetImage('lib/assets/imagetest.jpg'),
         //   ),
         // ),
-        backgroundColor: Colors.blueGrey[700],
+        backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            verticalDirection: VerticalDirection.down,
-            children: [
-              Container(
-                color: Colors.white,
-                child: Text("Container 1"),
-                // height: 100.0,
-                width: 220.0,
-                margin: EdgeInsets.fromLTRB(10, 50, 0, 0),
-                padding: EdgeInsets.all(25.0),
+            child: Row(
 
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                color: Colors.red,
-                child: Text("Container 2"),
-                // height: 100.0,
-                width: 220.0,
-                margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                padding: EdgeInsets.all(25.0),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                color: Colors.blue,
-                child: Text("Container 3"),
-                // height: 100.0,
-                width: 220.0,
-                margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
-                padding: EdgeInsets.all(25.0),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-            ],
-          ),
-        )
+              children: <Widget>[
+                new Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+
+                    Container(
+
+                      color: Colors.white,
+                      child: Text("Container 1"),
+                      alignment: Alignment.center,
+                      // height: 100.0,
+                      width: 100.0,
+                      margin: EdgeInsets.fromLTRB(0, 50, 0, 50),
+                      padding: EdgeInsets.all(25.0),
+
+                    ),
+
+                  ],
+
+                ),
+
+                new Row(
+
+                  children: [
+
+                    Container(
+                            color: Colors.red,
+                            child: Text("Container 2"),
+                            alignment: Alignment.center,
+                            height: 150.0,
+                            width: 100.0,
+                            margin: EdgeInsets.fromLTRB(76, 50, 0, 50),
+                            padding: EdgeInsets.all(20),
+                          ),
+
+                  ],
+
+                ),
+
+
+
+                new Row(
+                // mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+
+                    Container(
+                            color: Colors.blue,
+                            child: Text("Container 3"),
+                            // height: 100.0,
+                            width: 100.0,
+                            alignment: Alignment.center,
+                            margin: EdgeInsets.fromLTRB(72, 50, 0, 50),
+                            padding: EdgeInsets.all(25.0),
+                    )
+                  ],
+
+                )
+
+              ],
+            )
         ),
+      )
   ));
 }
+
+  // {    crossAxisAlignment: CrossAxisAlignment.center,
+  //   // verticalDirection: VerticalDirection.down,
+  //   children: [
+  //     Container(
+  //
+  //       color: Colors.white,
+  //       child: Text("Container 1"),
+  //       alignment: Alignment.center,
+  //       // height: 100.0,
+  //       width: 100.0,
+  //       margin: EdgeInsets.fromLTRB(0, 50, 0, 50),
+  //       padding: EdgeInsets.all(25.0),
+  //
+  //     ),
+  //     Container(
+  //       color: Colors.red,
+  //       child: Text("Container 2"),
+  //       alignment: Alignment.center,
+  //       // height: 100.0,
+  //       // width: 100.0,
+  //       margin: EdgeInsets.fromLTRB(0, 50, 0, 50),
+  //       padding: EdgeInsets.all(20),
+  //     ),
+  //     Container(
+  //       color: Colors.blue,
+  //       child: Text("Container 3"),
+  //       // height: 100.0,
+  //       width: 100.0,
+  //       alignment: Alignment.center,
+  //       margin: EdgeInsets.fromLTRB(0, 50, 0, 50),
+  //       padding: EdgeInsets.all(25.0),
+  //     ),
+  //   ],
+  // ),
