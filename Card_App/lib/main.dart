ba306d96700a68a348ff756eed2d787d992334f9
Sadra <alizadeh.sadra.gh@gmail.com>
2024-07-01 22:53:59 +0300

    Progress on Card App_2(8)

--- Card_App/lib/main.dart
+++ Card_App/lib/main.dart
@@ -1,24 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
       home: Scaffold(
-        // body: Center(
-        //   child: Image(
-        //     image: AssetImage('lib/assets/imagetest.jpg'),
-        //   ),
-        // ),
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Column(
-            children: <Widget>[
-
-              CircleAvatar()
-
-            ],
-          ),
-
-        ),
-      )
-  ));
+    // body: Center(
+    //   child: Image(
+    //     image: AssetImage('lib/assets/imagetest.jpg'),
+    //   ),
+    // ),
+    backgroundColor: Colors.teal,
+    body: SafeArea(
+      child: Column(
+        children: <Widget>[
+          CircleAvatar(
+            radius: 50.0,
+            backgroundImage: AssetImage("images2/ag.jpg"),
+          )
+        ],
+      ),
+    ),
+  )));
 }
