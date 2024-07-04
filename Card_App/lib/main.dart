8c29341445438617b2e15e07c55966c13501266a
Sadra <alizadeh.sadra.gh@gmail.com>
2024-07-04 16:56:06 +0300

    Add phone section_2(15)

--- Card_App/lib/main.dart
+++ Card_App/lib/main.dart
@@ -21,7 +21,7 @@ void main() {
             "Sadra Alizadeh",
             style: TextStyle(
               fontFamily: "DancingScript",
-              fontSize: 30.0,
+              fontSize: 40.0,
               color: Colors.black,
               fontWeight: FontWeight.bold,
             ),
@@ -30,9 +30,39 @@ void main() {
             "Computer Engineer",
             style: TextStyle(
               fontFamily: "Wittgenstein",
-              fontSize: 25.0,
+              fontSize: 20.0,
+              color: Colors.teal[100],
               letterSpacing: 2,
-              color: Colors.teal[800],
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Card(
+            color: Colors.white,
+            margin: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10.0),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Row(
+                children: <Widget>[
+                  const Icon(
+                    Icons.phone,
+                    // size: 30.0,
+                  ),
+                  const SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    "+90 123 456 7890",
+                    style: TextStyle(
+                      // fontFamily: "Wittgenstein",
+                      fontSize: 20.0,
+                      color: Colors.teal.shade900,
+              
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
             ),
           )
         ],
