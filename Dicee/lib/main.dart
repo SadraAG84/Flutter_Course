b7cda18f1a318dafa5015fabf9dbf54ebb081af5
Sadra <alizadeh.sadra.gh@gmail.com>
2024-07-08 22:41:07 +0300

    Progress on Dicee App_3(27)

--- Dicee/lib/main.dart
+++ Dicee/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(MaterialApp(
@@ -13,20 +14,35 @@ void main() {
   ));
 }
 
-class DicePage extends StatelessWidget {
+
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftdicenumber = 1;
+  int rightdicenumber = 1;
   @override
   Widget build(BuildContext context) {
+
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(child: TextButton(onPressed: () {
-            print("Left Button got pressed");
+            setState(() {
+              leftdicenumber = Random().nextInt(6) + 1;
+              print("number = $leftdicenumber");
+            });
           },
-          child: Image.asset("images/dice1.png"))),
+              child: Image.asset("images/dice$leftdicenumber.png"))),
           Expanded(child: TextButton(onPressed: () {
-            print("Right Button got presses");
+            setState(() {
+              rightdicenumber = Random().nextInt(6) + 1;
+              print("number = $rightdicenumber");
+            });
           },
-          child: Image.asset("images/dice2.png")))
+              child: Image.asset("images/dice$rightdicenumber.png")))
         ],
       ),
     );
