001e98712047b184f268523b75fdddfd95c3ef66
Sadra <alizadeh.sadra.gh@gmail.com>
2024-07-09 12:29:34 +0300

    Adding Images & Editing pubspec file & Progress_4(33)

--- Magic Q Ball/lib/main.dart
+++ Magic Q Ball/lib/main.dart
@@ -18,11 +18,27 @@ class BallPage extends StatelessWidget {
         title: Text("Ask Me Anything."),
 
       ),
-      body: Container(
+      body: Ball(
 
       ),
     );
 
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({super.key});
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset("images/ball1.png"),
+    );
   }
 }
 
