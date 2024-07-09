d7c041a8798b208e536b30c4e37c5d7451868ecf
Sadra <alizadeh.sadra.gh@gmail.com>
2024-07-09 21:10:31 +0300

    Start Xylophone Project_5(38)

---
+++ xylophone/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Xylophone());
+class Xylophone extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+            child: Container()
+        ),
+      ),
+    );
+  }
+}
+
+
