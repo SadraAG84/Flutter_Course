ffc8bad7c4658833d42439725d7b06d66d77a86a
Sadra <alizadeh.sadra.gh@gmail.com>
2024-07-10 16:57:14 +0300

    Adding musical keys_5(41)

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -1,34 +1,118 @@
 // import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+
 // import 'package:audioplayers/src/audio_cache.dart';
 import 'package:assets_audio_player/assets_audio_player.dart';
 
 void main() => runApp(Xylophone());
+
 class Xylophone extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-            child: Center(
-              child: TextButton(onPressed: () {
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                  child: TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(Colors.red),
+                      ),
+                      onPressed: () {
+                        AssetsAudioPlayer.newPlayer().open(
+                          Audio("assets/note1.wav"),
+                          autoStart: true,
+                          showNotification: true,
+                        );
+                      },
+                      child: Text(""))),
+              Expanded(
+                  child: TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(Colors.orange),
+                      ),
+                      onPressed: () {
+                        AssetsAudioPlayer.newPlayer().open(
+                          Audio("assets/note2.wav"),
+                          autoStart: true,
+                          showNotification: true,
+                        );
+                      },
+                      child: Text(""))),
+              Expanded(
+                  child: TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(Colors.yellow),
+                      ),
+                      onPressed: () {
+                        AssetsAudioPlayer.newPlayer().open(
+                          Audio("assets/note3.wav"),
+                          autoStart: true,
+                          showNotification: true,
+                        );
+                      },
+                      child: Text(""))),
+              Expanded(
+                  child: TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(Colors.green),
+                      ),
+                      onPressed: () {
+                        AssetsAudioPlayer.newPlayer().open(
+                          Audio("assets/note4.wav"),
+                          autoStart: true,
+                          showNotification: true,
+                        );
+                      },
+                      child: Text(""))),
+              Expanded(
+                  child: TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(Colors.teal),
+                      ),
+                      onPressed: () {
+                        AssetsAudioPlayer.newPlayer().open(
+                          Audio("assets/note5.wav"),
+                          autoStart: true,
+                          showNotification: true,
+                        );
+                      },
+                      child: Text(""))),
 
-                AssetsAudioPlayer.newPlayer().open(
-                  Audio("assets/note1.wav"),
-                  // autoStart: true,
-                  showNotification: true,
+              Expanded(
+                  child: TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(Colors.blue),
+                      ),
+                      onPressed: () {
+                        AssetsAudioPlayer.newPlayer().open(
+                          Audio("assets/note6.wav"),
+                          autoStart: true,
+                          showNotification: true,
+                        );
+                      },
+                      child: Text(""))),
 
-                );
 
+              Expanded(
+                  child: TextButton(
+                      style: ButtonStyle(
+                        backgroundColor: WidgetStatePropertyAll(Colors.purple),
+                      ),
+                      onPressed: () {
+                        AssetsAudioPlayer.newPlayer().open(
+                          Audio("assets/note7.wav"),
+                          autoStart: true,
+                          showNotification: true,
+                        );
+                      },
+                      child: Text(""))),
 
-              }, child: Text(
-                "Click to play sound."
-              ),),
-            )
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
-
