2cf17e12c9b3aaad42a320eb196d5a95f025c52f
Sadra <alizadeh.sadra.gh@gmail.com>
2024-07-16 13:11:07 +0300

    Clean Codes_7(67)

--- destini/lib/main.dart
+++ destini/lib/main.dart
@@ -14,7 +14,6 @@ class Destini extends StatelessWidget {
 }
 
 StoryBrain storyBrain = StoryBrain();
-//TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
 
 class StoryPage extends StatefulWidget {
   _StoryPageState createState() => _StoryPageState();
@@ -26,11 +25,10 @@ class _StoryPageState extends State<StoryPage> {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("images/background.png"),
-                fit: BoxFit.cover,
-          )
-        ),
+            image: DecorationImage(
+          image: AssetImage("images/background.png"),
+          fit: BoxFit.cover,
+        )),
         padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
         constraints: BoxConstraints.expand(),
         child: SafeArea(
@@ -42,7 +40,6 @@ class _StoryPageState extends State<StoryPage> {
                 child: Center(
                   child: Text(
                     storyBrain.getstory(),
-                    //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
                     style: TextStyle(
                       fontSize: 25.0,
                     ),
@@ -56,7 +53,8 @@ class _StoryPageState extends State<StoryPage> {
                     //Choice 1 made by user.
                     //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
                   },
-                  style: ButtonStyle(backgroundColor: WidgetStatePropertyAll(Colors.red)),
+                  style: ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(Colors.red)),
                   child: Text(
                     //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
                     'Choice 1',
@@ -78,8 +76,8 @@ class _StoryPageState extends State<StoryPage> {
                     //Choice 2 made by user.
                     //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
                   },
-                  style: ButtonStyle(backgroundColor: WidgetStatePropertyAll(Colors.blue)),
-
+                  style: ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(Colors.blue)),
                   child: Text(
                     //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
                     'Choice 2',
@@ -98,4 +96,3 @@ class _StoryPageState extends State<StoryPage> {
 }
 
 //TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
-
