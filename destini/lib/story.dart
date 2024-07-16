3cbffefe0884588979481848bbab86099a9e6b89
Sadra <alizadeh.sadra.gh@gmail.com>
2024-07-16 13:08:48 +0300

    Step 5/6/7/8 Done_7(65)

--- destini/lib/story.dart
+++ destini/lib/story.dart
@@ -1,9 +1,9 @@
-class Story{
+class Story {
 
   String storyTitle;
   String choice1;
   String choice2;
 
-  Story(this.storyTitle, this.choice1, this.choice2);
+  Story({required this.storyTitle, required this.choice1, required this.choice2});
 
 }
