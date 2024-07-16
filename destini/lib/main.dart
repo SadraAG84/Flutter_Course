0c6e1e8da3cfb684e9b65a63ebef2df5b4f10b31
Sadra <alizadeh.sadra.gh@gmail.com>
2024-07-16 13:09:21 +0300

    Step 9/10 Done_7(66)

--- destini/lib/main.dart
+++ destini/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'story_brain.dart';
 //TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
 
 void main() => runApp(Destini());
@@ -13,6 +13,7 @@ class Destini extends StatelessWidget {
   }
 }
 
+StoryBrain storyBrain = StoryBrain();
 //TODO: Step 9 - Create a new storyBrain object from the StoryBrain class.
 
 class StoryPage extends StatefulWidget {
@@ -40,8 +41,8 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 12,
                 child: Center(
                   child: Text(
+                    storyBrain.getstory(),
                     //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
-                    'Story text will go here.',
                     style: TextStyle(
                       fontSize: 25.0,
                     ),
