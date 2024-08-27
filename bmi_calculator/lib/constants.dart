e0180361ed83e8b8b5b7906c5d9b80921d623723
Sadra <alizadeh.sadra.gh@gmail.com>
2024-08-27 13:08:16 +0300

    Update constants.dart_8(94)

--- bmi_calculator/lib/constants.dart
+++ bmi_calculator/lib/constants.dart
@@ -8,3 +8,19 @@ const kBottomContainerColor = Color(0xFFEB1555);
 const kLabelTextStyle = TextStyle(fontSize: 18.0, color: Color(0xFF8D8E98));
 
 const kNumberTextStyle = TextStyle(fontSize: 50.0, fontWeight: FontWeight.w900);
+
+const kTitleTextStyle = TextStyle(
+    fontSize: 45.0,
+    fontWeight: FontWeight.bold
+);
+
+const kKeyResultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 85.0,
+  fontWeight: FontWeight.bold
+);
